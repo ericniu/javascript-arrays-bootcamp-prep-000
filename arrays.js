2036b63b1433b053408bae4bd23eab65bef243ef
Eric Niu <eric@goswaggle.com>
2018-02-03 03:20:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,15 @@ function destructivelyAddElementToBeginningOfArray (array,element){
   return array
 destructivelyAddElementToBeginningOfArray (["a","b","c"])
 }
+
+function addElementToEndOfArray (array,element){
+  array=[element,...array]
+  return array
+}
+addElementToEndOfArray (["a","b","c"])
+
+function destructivelyAddElementToEndOfArray (aaray,element){
+  array=[...array,element]
+  return array
+}
+destructivelyAddElementToEndOfArray (["a","b","c"])
